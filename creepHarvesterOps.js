b480cc016162c359a2bd326bc736482bba21bc15
jerdaz <jf.cuppen@gmail.com>
2019-06-15 22:57:56 +0200

    'x'

--- creepHarvesterOps.js
+++ creepHarvesterOps.js
@@ -8,7 +8,7 @@ function main(creep) {
 
 function strategy(creep) {
     if (!creep.command) creepOps.harvest(creep
-                                    , creep.pos.findClosestByPath(FIND_ACTIVE_SOURCE)
+                                    , creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
                                     , creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_SPAWN}} )
                                     );
 }
