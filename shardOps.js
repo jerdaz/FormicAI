4a1236d7522b24dfcfef482862fa26e209c5ade1
jerdaz <jf.cuppen@gmail.com>
2019-06-15 21:50:53 +0200

    'x'

--- shardOps.js
+++ shardOps.js
@@ -11,8 +11,8 @@ function main() {
 
     function getBases(rooms) {
         let bases = _.filter(rooms, (o) => { return o.controller && o.controller.my});
-        for (base of bases) base.creeps = [];
-        for (creep of creeps) {
+        for (let base of bases) base.creeps = [];
+        for (let creep of creeps) {
             let roomName = creep.name.split('_')[0];
             let base = bases[roomName];
             if (base) base.creeps.concat(creep);
