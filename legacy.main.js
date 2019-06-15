b13bb4d0af553c00ca634a929c9bbc8fed3b389c
jerdaz <jf.cuppen@gmail.com>
2019-06-15 20:21:26 +0200

    x

--- legacy.main.js
+++ legacy.main.js
@@ -7085,5 +7085,5 @@ const loop$1 = ErrorMapper.wrapLoop(() => {
     screepsProfiler_1(function () { myMain(); });
 });
 
-exports.loop = loop$1;
+exports.loop = myMain();
 //# sourceMappingURL=main.js.map
