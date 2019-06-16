a1c93b9476d79819597586ca774203e9c3d1bf99
jerdaz <jf.cuppen@gmail.com>
2019-06-16 09:51:00 +0200

    'x'

--- baseOps.js
+++ baseOps.js
@@ -13,7 +13,7 @@ function main(base) {
         }
     }
 
-    if (Math.floor(Math.random() * 1) == 0) strategy(base);
+    if (Math.floor(Math.random() * 10) == 0) strategy(base);
     
     command(base);
 }
