3300e1d2934ef2c9bd099d456929fbdfb4df309d
jerdaz <jf.cuppen@gmail.com>
2019-06-16 08:46:55 +0200

    'x'

--- structSpawnOps.js
+++ structSpawnOps.js
@@ -11,7 +11,7 @@ function strategy() {
 function command(spawn) {
     switch(spawn.command) {
         case 'spawnHarvester':
-            spawn.spawnCreep([WORK,MOVE,CARRY], spawn.room.name + '_' + Math.random()) , {memory: {role: 'harvester'}};
+            spawn.spawnCreep([WORK,MOVE,CARRY], spawn.room.name + '_' + Math.random() , {memory: {role: 'harvester'}});
             break;
     }
 }
