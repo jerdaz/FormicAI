211988d99bac709e0c27fd9300687ebef678cbdf
jerdaz <jf.cuppen@gmail.com>
2019-06-16 13:07:03 +0200

    'x'

--- creepBuilderOps.js
+++ creepBuilderOps.js
@@ -7,7 +7,7 @@ function main(creep) {
 }
 
 function strategy(creep) {
-    if (creep.memory.command == undefined) creepOps.harvest(creep
+    if (creepOps.getCommand(creep) == undefined) creepOps.harvest(creep
                                     , creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
                                     , creep.pos.findClosestByPath(FIND_MY_CONSTRUCTION_SITES)
                                     );
