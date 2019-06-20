2362bf504c8a387ea24bed911cd5835d78b3fcb3
jerdaz <jf.cuppen@gmail.com>
2019-06-20 19:58:41 +0200

    'x'

--- main.js
+++ main.js
@@ -7,16 +7,7 @@ class Main extends Operation {
         super();
         U.l('INIT MAIN');
         for (let memObj in Memory) {
-            switch (memObj) {
-                case 'creep':
-                case 'room':
-                case 'flag':
-                case 'spawn':
-                    Memory[memObj] = {};
-                    break;
-                default:
-                    delete Memory[memObj];
-            }
+            delete Memory[memObj];
         }
         this._shardOp = new ShardOp();
     }
