cda799cd1e3267710a17429d5ec19f89e2a6de2a
jerdaz <jf.cuppen@gmail.com>
2019-06-21 21:52:37 +0200

    'x'

--- teamColonizingOp.js
+++ teamColonizingOp.js
@@ -6,7 +6,7 @@ module.exports = class CreepTeamColonizingOp extends CreepTeamOp {
     _strategy() {
         let nCreep = 0;
         if (this._baseOp.getDirective() == c.DIRECTIVE_COLONIZE) nCreep = 1;
-        this._spawningOp.ltRequestSpawn(c.OPERATION_COLONIZING, {body:[WORK,MOVE,CARRY]}, nCreep)
+        this._spawningOp.ltRequestSpawn(c.OPERATION_COLONIZING, {body:[MOVE,CLAIM], maxLength: 2, minLength:2}, nCreep)
 
         for (let creepName in this._creepOps) {
             let creepOp = this._creepOps[creepName];
