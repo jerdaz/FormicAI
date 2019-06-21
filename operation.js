10b772d1ebdc054eed5d7a1f6eef04dda599b249
jerdaz <jf.cuppen@gmail.com>
2019-06-21 22:09:53 +0200

    'x'

--- operation.js
+++ operation.js
@@ -13,13 +13,12 @@ module.exports = class Operation {
     run() {
         // @ts-ignore
         if(Game.debug.verbose) U.l (this);
-        let err;
         try {
             this._strategy();
-        } catch(error) {U.log_error(error)};
+        } catch(err) {U.log_error(err)};
         try {
             this._command();
-        } catch(error) {U.log_error(error)};
+        } catch(err) {U.log_error(err)};
         if (this._firstRun) this._firstRun = false;
     }
 
