062b1b18caa0b3cc99c6a640251317055763adfc
jerdaz <jf.cuppen@gmail.com>
2019-06-23 10:00:43 +0200

    'x'

--- shardOp.js
+++ shardOp.js
@@ -66,7 +66,7 @@ module.exports = class ShardOp extends Operation {
     _strategy(){
         if (U.chance(100)) {
             let directive = c.DIRECTIVE_NONE;
-            if (Game.cpu.bucket >= CPU_MAX_BUCKET && Game.gcl > _.size(this._baseOps)) directive = c.DIRECTIVE_COLONIZE
+            if (Game.cpu.bucket >= CPU_MAX_BUCKET && Game.gcl.level > _.size(this._baseOps)) directive = c.DIRECTIVE_COLONIZE
             for (let baseName in this._baseOps) this._baseOps[baseName].setDirective(directive);
         }
     }
