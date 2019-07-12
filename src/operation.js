6e8bc4cd2e3e5c69506b864e934f19f64d7aa271
jerdaz <jf.cuppen@gmail.com>
2019-07-12 07:44:25 +0200

    x

--- src/operation.js
+++ src/operation.js
@@ -10,13 +10,15 @@ module.exports = class Operation {
     constructor() {
         this._id = idIndex++;
         this._firstRun = true;
+        /**@type {Operation[]} */
+        this._subOps = []
         /**@type {Debug} */
         this._debug = /** @type {any}*/(Game).debug;
     }
 
     run() {
         //last resort cpu overflow prevention.
-        if (Game.cpu.bucket < Game.cpu.getUsed() + Game.cpu.limit) return;
+        if (Game.cpu.bucket < Game.cpu.getUsed() + Game.cpu.limit) throw Error('Out of CPU');
 
         if(this._debug.verbose) this._debug.logState('support', this)
         try {
@@ -30,8 +32,18 @@ module.exports = class Operation {
         try {
             this._command();
         } catch(err) {this._debug.logError(err)};
+        for (let subOp of this._subOps) {
+            try {
+                subOp.run();
+            } catch(err) {this._debug.logError(err)}
+        }
         if (this._firstRun) this._firstRun = false;
         if(this._debug.verbose) this._debug.logState('end', this)
+    }
+
+    /**@param {Operation} subOp */
+    _addSubOp(subOp) {
+        this._subOps.push(subOp);
     }
 
     _support() {}
