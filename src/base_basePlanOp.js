450b27cfacb54041473e61a24b0be1e04dd62573
jerdaz <jf.cuppen@gmail.com>
2019-10-24 20:26:21 +0200

    fix building error

--- src/base_basePlanOp.js
+++ src/base_basePlanOp.js
@@ -121,6 +121,8 @@ module.exports = class BasePlanOp extends BaseChildOp{
         if (!base.controller) throw Error();
         if (x<2 || x > 47 || y < 2 || y > 47) return false;
         let pos = new RoomPosition(x, y, base.name)
+        let terrain = pos.lookFor(LOOK_TERRAIN);
+        if (_.find(terrain,{type: TERRAIN_MASK_WALL})) return false;
         let structures = pos.lookFor(LOOK_STRUCTURES);
         let countStructures = 0;
         for (var i=0;i<structures.length;i++) if (structures[i].structureType != STRUCTURE_ROAD) countStructures++;
