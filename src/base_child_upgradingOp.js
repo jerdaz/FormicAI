2e15d0c7b3fdb758ed1d6efae534bd25d8f5bfd8
jerdaz <jf.cuppen@gmail.com>
2019-11-03 08:49:15 +0100

    reduce upgraders to 1 at EOL phase

--- src/base_child_upgradingOp.js
+++ src/base_child_upgradingOp.js
@@ -20,7 +20,7 @@ module.exports = class UpgradingOp extends BaseChildOp {
             let energy = this.baseOp.storage.store.energy;
             let workerCount = Math.floor((energy - ENERGY_RESERVE ) / (MAX_CREEP_SIZE / 3 * UPGRADE_CONTROLLER_POWER * CREEP_LIFE_TIME))
             if (workerCount < 0) workerCount = 0;
-            if (this.baseOp.phase >= c.BASE_PHASE_EOL && workerCount > 2) workerCount = 2
+            if (this.baseOp.phase >= c.BASE_PHASE_EOL && workerCount > 2) workerCount = 1
             if (workerCount < 1 && this.baseOp.base.controller.ticksToDowngrade < CONTROLLER_DOWNGRADE[1]*DOWNGRADE_RESERVE) workerCount = 1;
             this.baseOp.spawningOp.ltRequestSpawn(this, {body:[MOVE,CARRY,WORK]}, workerCount)
         }
