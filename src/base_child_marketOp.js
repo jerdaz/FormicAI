574fdec75586ae8ec0e727a3908509f57545bde9
Jerdaz <jf.cuppen@gmail.com>
2020-03-22 14:43:34 +0100

    update

--- src/base_child_marketOp.js
+++ src/base_child_marketOp.js
@@ -4,12 +4,18 @@ const BaseChildOp = require('./base_baseChildOp');
 
 const MIN_MARKET_CREDITS = 10;
 const MIN_STOCK_PILE_SIZE = Math.floor(MAX_CREEP_SIZE / 3) * CARRY_CAPACITY * 2 //this is equal to 1 full transporter creep;
+const TERMINAL_STORAGE = {
+    RESOURCE_ENERGY : 10,
+    RESOU
+}
+
 
 module.exports = class MarketOp extends BaseChildOp {
     /**@param {BaseOp} baseOp */
     constructor(baseOp) {
         super(baseOp);
-        this._energyPrice = 0;
+        /**@type {{[index:string]:number}} */
+        this._resourcePrice = {};
         this._verbose = false;
     }
 
@@ -58,16 +64,17 @@ module.exports = class MarketOp extends BaseChildOp {
         }        
 
         // sell minerals
+        let energyPrice = this._resourcePrice[RESOURCE_ENERGY] || market.getHistory(RESOURCE_ENERGY)[0].avgPrice || 0;
         for (let resourceName in terminal.store) {
             let resourceType = /**@type {ResourceConstant} */ (resourceName);
             if (resourceType == RESOURCE_ENERGY) continue;
             let amount = terminal.store[resourceType];
-            this._log({base: baseOp.name, Trying_to_sell: resourceType, amount:amount, energyPrice: this._energyPrice})
+            //this._log({base: baseOp.name, Trying_to_sell: resourceType, amount:amount, energyPrice: this._energyPrice})
             /**@type {OrderEx[]} */
             let orders = market.getAllOrders({type:ORDER_BUY, resourceType: resourceType});
             //calculate net price
             for (let order of orders) {
-                order.transactionCost = this._energyPrice * market.calcTransactionCost(1,order.roomName||this._baseOp.name, this._baseOp.name);
+                order.transactionCost = energyPrice * market.calcTransactionCost(1,order.roomName||this._baseOp.name, this._baseOp.name);
                 order.netPrice = order.price - order.transactionCost;
             }
             //sort high to low price
@@ -87,12 +94,13 @@ module.exports = class MarketOp extends BaseChildOp {
              }
         }
 
-        // buy energy
+        // buy resources
+        let buyType = RESOURCE_ENERGY;
         let credits = this._baseOp.credits;
         if (credits > MIN_MARKET_CREDITS) {
-            this._log({base: baseOp.name, Trying_to_buy: 'energy', energyPrice: this._energyPrice})
+            //this._log({base: baseOp.name, Trying_to_buy: 'energy', energyPrice: this._energyPrice})
             /**@type {OrderEx[]} */
-            let orders = market.getAllOrders({type:ORDER_SELL, resourceType: RESOURCE_ENERGY})
+            let orders = market.getAllOrders({type:ORDER_SELL, resourceType: buyType})
             //calculate net price
             for (let order of orders) {
                 order.transactionCost = market.calcTransactionCost(1000,order.roomName||this._baseOp.name, this._baseOp.name)/1000;
@@ -114,7 +122,7 @@ module.exports = class MarketOp extends BaseChildOp {
             }
             //calculate and save current local energyprice
             if (orders[0]) {
-                this._energyPrice = orders[0].price / (1-market.calcTransactionCost(1000,orders[0].roomName||this._baseOp.name, this._baseOp.name)/1000);
+                this._resourcePrice[buyType] = orders[0].price / (1-market.calcTransactionCost(1000,orders[0].roomName||this._baseOp.name, this._baseOp.name)/1000);
             }
         }
     }
