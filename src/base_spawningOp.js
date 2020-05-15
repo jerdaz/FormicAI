ae899ad1281c37a84b30eb3b457a6012093cd6f6
jerdaz <43273220+jerdaz@users.noreply.github.com>
2020-05-15 20:45:33 +0200

    increase colonizer spawn priority

--- src/base_spawningOp.js
+++ src/base_spawningOp.js
@@ -61,7 +61,7 @@ module.exports = class SpawningOp extends BaseChildOp {
             this._spawnPrio[c.OPERATION_LINK] = 40;
             this._spawnPrio[c.OPERATION_BUILDING] = 20;
             this._spawnPrio[c.OPERATION_UPGRADING] = 2;
-            this._spawnPrio[c.OPERATION_COLONIZING] = 10;
+            this._spawnPrio[c.OPERATION_COLONIZING] = 75;
             this._spawnPrio[c.OPERATION_SCOUTING] = 1;
         }
     }
