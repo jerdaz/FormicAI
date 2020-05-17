facc21858be54aebb535f28adfa89d9f9b148357
jerdaz <43273220+jerdaz@users.noreply.github.com>
2020-05-17 19:16:47 +0200

    spawn tweak

--- src/base_spawningOp.js
+++ src/base_spawningOp.js
@@ -4,7 +4,7 @@ const BaseChildOp = require('./base_childOp');
 
 /**@type {{[body:string]:number}} */
 const BODY_SORT = {'tough': 1, 'move': 2, 'carry': 3, 'work': 4 , 'claim': 5, 'attack': 6, 'ranged_attack': 7, 'heal': 8};
-const MAX_OPERATION_IDLE_TIME = 50;
+const MAX_OPERATION_IDLE_TIME = 25;
 
 module.exports = class SpawningOp extends BaseChildOp {
     /**@param {BaseOp} baseOp */
