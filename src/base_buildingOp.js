bfef93cb1f1a6efeb95183a0e39e20bc6c38657b
jerdaz <jf.cuppen@gmail.com>
2020-06-23 13:34:07 +0200

    fix

--- src/base_buildingOp.js
+++ src/base_buildingOp.js
@@ -47,7 +47,9 @@ module.exports = class BuildingOp extends BaseChildOp {
             let creepOp = this._creepOps[creepName];
             if (creepOp.instruction == c.COMMAND_NONE && creepOp.pos.roomName != this._baseOp.name) creepOp.instructMoveTo(this._baseOp.centerPos);
             else if (creepOp.instruction == c.COMMAND_NONE && constructionCount == 0) creepOp.instructUpgradeController(this._baseOp.name);
-            else if (creepOp.instruction != c.COMMAND_BUILD) creepOp.instructBuild()
+            else if (creepOp.instruction != c.COMMAND_BUILD && constructionCount > 0) {
+                creepOp.instructBuild()
+            }
         }
     }
 }
