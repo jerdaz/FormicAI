67408ee2bc7230b4096cc2f04f539050d54a87e1
jerdaz <43273220+jerdaz@users.noreply.github.com>
2020-09-05 12:19:52 +0200

    fix

--- src/shard_spawningOp.js
+++ src/shard_spawningOp.js
@@ -24,19 +24,21 @@ module.exports = class ShardSpawningOp extends ShardChildOp {
         //determin new base for shard spawning
         let baseOps = this._shardOp.baseOps;
         let baseOp = this._shardOp.getBaseOp(this._spawnBase);
-        if (!baseOp) throw Error();
-        let oldSpawningOp = baseOp.spawningOp;
+        /**@type {SpawningOp|null} */
+        let oldSpawningOp = null;
+        /**@type {SpawningOp|null} */
+        let newSpawningOp = null;
+        if (baseOp) oldSpawningOp = baseOp.spawningOp;
         this._spawnBase = baseOps.keys().next().value;
         baseOp = this._shardOp.getBaseOp(this._spawnBase);
-        if (!baseOp) throw Error();
-        let newSpawningOp = baseOp.spawningOp;
+        if (baseOp) newSpawningOp = baseOp.spawningOp;
 
         // if new spawning op is not equal, move the requests to the new spawning base
         if (oldSpawningOp != newSpawningOp) {
             for (let spawnRequestId in this._spawnRequests) {
                 let spawnRequest = this._spawnRequests[spawnRequestId];
-                oldSpawningOp.ltRequestSpawn(this._shardOp.getOp(spawnRequest.operationId), spawnRequest.template, 0)
-                newSpawningOp.ltRequestSpawn(this._shardOp.getOp(spawnRequest.operationId), spawnRequest.template, spawnRequest.count)
+                if (oldSpawningOp) oldSpawningOp.ltRequestSpawn(this._shardOp.getOp(spawnRequest.operationId), spawnRequest.template, 0)
+                if (newSpawningOp) newSpawningOp.ltRequestSpawn(this._shardOp.getOp(spawnRequest.operationId), spawnRequest.template, spawnRequest.count)
             }
         }
     }
