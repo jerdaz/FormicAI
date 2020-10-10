00443d19156f91fcaa5cfce0c11a4f97ae079e4f
jerdaz <43273220+jerdaz@users.noreply.github.com>
2020-10-10 20:13:33 +0200

    fix

--- src/base_upgradingOp.js
+++ src/base_upgradingOp.js
@@ -67,7 +67,9 @@ module.exports = class UpgradingOp extends BaseChildOp {
             } else {
                 if (creepOp.instruction != c.COMMAND_UPGRADE) 
                 {
-                    creepOp.instructUpgradeController(this._baseOp.name);
+                    let link = this.baseOp.base.controller.pos.findInRange(FIND_MY_STRUCTURES, 2,  {filter: {structureType: STRUCTURE_LINK}})[0]
+                    if (link) creepOp.instructTransfer(link, this.baseOp.base.controller)
+                    else creepOp.instructUpgradeController(this._baseOp.name);
                 }
             }
         }
