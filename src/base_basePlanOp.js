ff2a815112017e990e4cd85f5a14a4b87c653007
Jeroen <jf.cuppen@gmail.com>
2021-05-16 13:51:26 +0200

    fix minimum wallheight

--- src/base_basePlanOp.js
+++ src/base_basePlanOp.js
@@ -69,7 +69,8 @@ module.exports = class BasePlanOp extends BaseChildOp{
         }
         let roomLevel = 1;
         if (this._baseOp) roomLevel = this._baseOp.level
-        this._maxWallHeight = Math.min(c.MAX_WALL_HEIGHT * RAMPART_HITS_MAX[this._baseOp.level], minHeight*2);                    
+        this._maxWallHeight = Math.min(c.MAX_WALL_HEIGHT * RAMPART_HITS_MAX[this._baseOp.level], minHeight*2);       
+        if (this._maxWallHeight <= RAMPART_DECAY_AMOUNT * 2) this._maxWallHeight = RAMPART_DECAY_AMOUNT * 2 + 1;            
 
 
         //find & destroy improperly placed buildings.
