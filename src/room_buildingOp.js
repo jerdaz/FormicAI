ed32f072e53d817c11274943fb7314766357e633
jerdaz <43273220+jerdaz@users.noreply.github.com>
2021-09-03 12:18:22 +0200

    fix

--- src/room_buildingOp.js
+++ src/room_buildingOp.js
@@ -83,7 +83,10 @@ module.exports = class BuildingOp extends RoomChildOp {
             if (creep.pos.roomName != room.name) creepOp.instructMoveTo(room.name);
             else if (room.name == this._baseOp.name && !this._buildWork) creepOp.instructUpgradeController(this._baseOp.name);
             else if (room.controller && room.controller.my && room.controller.level <= 1) creepOp.instructUpgradeController(room.name);
-            else if (!this._buildWork && this._baseOp.name != this._roomOp.name) creepOp.newParent(this._baseOp.buildingOp); //reassign to base building op if current subroom doesn't have build work
+            else if (!this._buildWork && this._baseOp.name != this._roomOp.name) {
+                creepOp.newParent(this._baseOp.buildingOp); //reassign to base building op if current subroom doesn't have build work
+                this._strategy(); // update number of requested creeps;            }
+            }
             else if (creepOp.instruction != c.COMMAND_BUILD && creepOp.pos.roomName == this._roomOp.roomName && constructionSites.length>0) { //stop upgrading if there are construction sites
                 creepOp.instructBuild()
             }
