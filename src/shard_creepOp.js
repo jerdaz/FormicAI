c01901548e94cc9a1a634286380a73c27653db60
jerdaz <jf.cuppen@gmail.com>
2022-02-05 19:34:29 +0100

    x

--- src/shard_creepOp.js
+++ src/shard_creepOp.js
@@ -351,7 +351,7 @@ module.exports = class CreepOp extends ChildOp {
 
         if (creep.store.getFreeCapacity(RESOURCE_ENERGY) - (mutations[creep.id]||0) <= 0) {
             this._state = c.STATE_OUTPUT 
-            //if (this._instruct == c.COMMAND_FILL) this._sourceId = '';
+            if (this._instruct == c.COMMAND_FILL) this._sourceId = '';
         }
 
         return result;
@@ -404,11 +404,11 @@ module.exports = class CreepOp extends ChildOp {
         mutations[target.id] = (mutations[target.id]||0) + amount;
         mutations[creep.id] = (mutations[creep.id]||0) - amount;
 
-        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) - (mutations[creep.id]||0) <= 0) this._state = c.STATE_INPUT;
-        //  {
-        //     this._state = c.STATE_INPUT;
-        //     if (this._instruct == c.COMMAND_FILL) this._destId = '';
-        // }
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) + (mutations[creep.id]||0) <= 0) this._state = c.STATE_INPUT;
+         {
+            this._state = c.STATE_INPUT;
+            if (this._instruct == c.COMMAND_FILL) this._destId = '';
+        }
         return result;
     }  
 
