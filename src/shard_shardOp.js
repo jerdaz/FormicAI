ba7c6bc54d3a12daa5683a1168c1e352ca5f21a7
Jeroen <jf.cuppen@gmail.com>
2022-02-08 17:47:36 +0100

    fix

--- src/shard_shardOp.js
+++ src/shard_shardOp.js
@@ -133,7 +133,7 @@ module.exports = class ShardOp extends ChildOp {
 
     /**@param {String} roomName */
     requestBuilder(roomName){
-        let donorRoom = this._map.findClosestBaseByPath(roomName, 4 , true, 20);
+        let donorRoom = this._map.findClosestBaseByPath(roomName, 4 , true, 1500, 20);
         if (!donorRoom) return;
         let baseOp = this._baseOpsMap.get(donorRoom);
         if (!baseOp) throw Error('donorroom not in basemap');
