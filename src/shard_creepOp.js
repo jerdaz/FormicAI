c409931293085e4906e272fedade003a92dcedf7
Jeroen <jf.cuppen@gmail.com>
2022-02-11 19:57:49 +0100

    fix

--- src/shard_creepOp.js
+++ src/shard_creepOp.js
@@ -357,7 +357,7 @@ module.exports = class CreepOp extends ChildOp {
 
         if (creep.store.getFreeCapacity(RESOURCE_ENERGY) - (mutations[creep.id]||0) <= 0) {
             this._state = c.STATE_OUTPUT 
-            if (this._instruct == c.COMMAND_FILL && !(source instanceof Source)) this._sourceId = '';
+            if (this._instruct == c.COMMAND_FILL /* && !(source instanceof Source)*/) this._sourceId = '';
         }
 
         return result;
