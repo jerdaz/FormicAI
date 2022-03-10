67211c8c8298ae7f71bb7812eb21dc0ac44b2cab
jerdaz <jf.cuppen@gmail.com>
2022-03-10 11:28:34 +0100

    fix

--- src/room_buildingOp.js
+++ src/room_buildingOp.js
@@ -27,7 +27,7 @@ module.exports = class BuildingOp extends RoomChildOp {
         if (repairSites.length > 0 || constructionSites.length >0 ) buildWork = true;
         
         let roomInfo = this._map.getRoomInfo(this._roomName)
-        if (roomInfo && (
+        if (roomInfo && (roomInfo.safeMode||0) == 0 && (
             roomInfo.lastSeenAttacker >= Game.time - 1500
             || roomInfo.activeTowers >= 1
             || roomInfo.invasion == true
